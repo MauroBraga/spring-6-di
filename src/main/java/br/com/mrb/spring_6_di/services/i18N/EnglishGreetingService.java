a82cafafba88329d992989755f5e57a3823e51bc
mauro <mrb0305@gmail.com>
2024-07-06 12:30:50 -0300

    Spring Profiles

--- src/main/java/br/com/mrb/spring_6_di/services/i18N/EnglishGreetingService.java
+++ src/main/java/br/com/mrb/spring_6_di/services/i18N/EnglishGreetingService.java
@@ -4,7 +4,7 @@ import br.com.mrb.spring_6_di.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("i18NService")
 public class EnglishGreetingService  implements GreetingService {
     @Override
